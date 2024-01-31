d173ed5edb444b9207a5a05307dc8483fddad7e7
TetrisBattle <thien5596@gmail.com>
2024-01-31 07:49:29 +0200

    useSetupTheme in useEffect

--- src/hooks/useSetupTheme.tsx
+++ src/hooks/useSetupTheme.tsx
@@ -1,29 +1,31 @@
+import { useEffect } from 'react'
 import { useThemeStore } from '@thng/react'
 
 export const useSetupTheme = () => {
 	const themeStore = useThemeStore()
-
 	const red = '#820000'
 	const yellow = '#EBD265'
 
-	themeStore.setLightThemeOptions({
-		palette: {
-			primary: {
-				main: red,
-				contrastText: yellow,
-			},
-			secondary: {
-				main: yellow,
-			},
-			background: {
-				paper: red,
-				default: red,
-			},
-			text: {
-				primary: yellow,
-			},
-		},
-	})
+	useEffect(() => {
+		themeStore.setIsDarkMode(false)
 
-	themeStore.setIsDarkMode(false)
+		themeStore.setLightThemeOptions({
+			palette: {
+				primary: {
+					main: red,
+					contrastText: yellow,
+				},
+				secondary: {
+					main: yellow,
+				},
+				background: {
+					paper: red,
+					default: red,
+				},
+				text: {
+					primary: yellow,
+				},
+			},
+		})
+	}, [themeStore])
 }
